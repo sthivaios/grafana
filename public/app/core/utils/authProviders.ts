7facb3655c02bbd50da783945c4e62e797d845ac
Stratos Thivaios <me@sthivaios.dev>
2026-06-05 15:01:56 +0300

    refactor: rename `getUserOAuthProviderDisplayName` to `getOAuthProviderDisplayName` for clarity

--- public/app/core/utils/authProviders.ts
+++ public/app/core/utils/authProviders.ts
@@ -1,6 +1,6 @@
 import config from 'app/core/config';
 
-export default function getUserOAuthProviderDisplayName(authSource: string | 0 | undefined): string {
+export default function getOAuthProviderDisplayName(authSource: string | 0 | undefined): string {
   if (authSource !== undefined && authSource !== 0) {
     return authSource === 'Generic OAuth' ? (config.oauth.generic_oauth?.name ?? '') : authSource;
   } else {
